cdd7b47393c222313497a6de59f9f4e448b87745
FernandoGmz2001 <fernandogmz2001@gmail.com>
2023-09-19 10:08:51 -0500

    feat: added validations

--- Examen/Ejercicio2.py
+++ Examen/Ejercicio2.py
@@ -3,16 +3,28 @@
 #Entradas n1 = 734 n2 = 893 SALIDA : 437
 #Entradas n1 = 221 n2 = 231
 
-num1  = (input("Ingrese el primer numero: "))
-num2 = (input("Ingrese el segundo numero: "))
+# Entradas
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
 
-num1 = num1[::-1]
-num2 = num2[::-1]
-num1 = int(num1)
-num2 = int(num2)
-if(num1> num2):
+# Validación de longitud
+if len(num1) != 3 or len(num2) != 3:
+    print("Los números deben tener 3 dígitos cada uno.")
+    exit()
+
+# Validación de caracteres numéricos
+if not num1.isdigit() or not num2.isdigit():
+    print("Los números deben ser enteros.")
+    exit()
+
+# Invertir los números
+num1 = int(num1[::-1])
+num2 = int(num2[::-1])
+
+# Comparar y mostrar resultado
+if num1 > num2:
     print(num1)
-elif(num1 == num2):
-    print("Son iguales")
+elif num1 == num2:
+    print("Los números son iguales.")
 else:
     print(num2)
