5c2a195ffb2500404e1d45c102d11e3956a64a80
PipeMtz <felipilloto2@gmail.com>
2023-09-21 09:49:46 -0500

    Ejercicio6

--- Examen/Ejercicio6(Extra).py
+++ Examen/Ejercicio6(Extra).py
@@ -2,24 +2,30 @@
 import sys
 print(sys.stdout.encoding)
 
+import re
+
 def contar_palabras():
     insertedWord = input('Ingrese la palabra a buscar: ')
     archivo = open("ArchivoTexto.txt")
     texto = archivo.read()
     texto = texto.rstrip()
     print(texto)
-    separatedWords = texto.split(' ')
+    
+    # Utilizar una expresión regular para dividir el texto en palabras
+    separatedWords = re.findall(r'\b\w+\b', texto.lower())
+    
     contadorPalabrasRepetidas = 0
     print(separatedWords)
     positionList = []
-    for (index,word) in enumerate(separatedWords):
-        if word == insertedWord:
+    for (index, word) in enumerate(separatedWords):
+        if word == insertedWord.lower():
             positionList.append(index)
             contadorPalabrasRepetidas += 1
+    
     if contadorPalabrasRepetidas == 0:
         print('La palabra no se encontró en el texto.')
     else:
-        print('Las posiciones donde se encuentra la palabra son: ',positionList)
-        print('La palabra se repite ',contadorPalabrasRepetidas,' veces')
+        print('Las posiciones donde se encuentra la palabra son: ', positionList)
+        print('La palabra se repite ', contadorPalabrasRepetidas, ' veces')
 
 contar_palabras()
